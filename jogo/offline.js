9925a4aaa4b6151552a36ca28ac91b9ac6de32b4
William de Sousa Costa <37248765+william2308@users.noreply.github.com>
2022-02-06 12:07:11 -0300

    adicionado os arquivos do jogo

---
+++ jogo/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1565736804,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tilemap.png",
+		"images/jogador-sheet0.png",
+		"images/portal-sheet0.png",
+		"images/grama_andar.png",
+		"images/luz_jogador-sheet0.png",
+		"images/lampada-sheet0.png",
+		"images/luz_fogo-sheet0.png",
+		"images/particulas_fogo.png",
+		"images/plataf_cair-sheet0.png",
+		"images/plataf_cair-sheet1.png",
+		"images/preto.png",
+		"images/particulasluz.png",
+		"images/particulasluz2.png",
+		"images/butaodireita-sheet0.png",
+		"images/butaoesquerda-sheet0.png",
+		"images/butaopulo-sheet0.png",
+		"images/butao-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/inicialfundo-sheet0.png"
+	]
+}
